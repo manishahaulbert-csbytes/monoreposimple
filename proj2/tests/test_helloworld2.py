5e959bb6814609afdb838145ea8e0d0ffd07aa2a
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-04-29 18:45:15 +0200

    proj2

--- proj2/tests/test_helloworld2.py
+++ proj2/tests/test_helloworld2.py
@@ -9,3 +9,10 @@ def test_not_helloworld2():
     print("Running test_not_helloworld2")
     assert hw2.j2 != "goodbye world"
     print(f"Value of j2: {hw2.j2}")
+
+
+def test_helloworld2_not_helloworld1():
+    print("Running test_helloworld2_not_helloworld1")
+    print(f"Value of j2: {hw2.j2}")
+    print("This test should fail because j2 is not equal to 'hello world1'")
+    assert hw2.j2 != "hello world1"
