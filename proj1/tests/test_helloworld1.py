2c2466348f18c580a730cc212e0b5793eef13ecd
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-04-29 18:51:08 +0200

    fail quality gate

--- proj1/tests/test_helloworld1.py
+++ proj1/tests/test_helloworld1.py
@@ -5,13 +5,13 @@ def test_helloworld1():
 
 def test_not_helloworld1():
     print("Running test_not_helloworld1")
-    assert hw.j1 != "goodbye world"
+    assert hw.j1 <> "goodbye world"
 
 def test_helloworld1_not_helloworld2():
     print("Running test_helloworld1_not_helloworld2")
     print(f"Value of j1: {hw.j1}")
     print("This test should fail because j1 is not equal to 'hello world2'")
-    assert hw.j1 != "hello world2"
+    assert hw.j1 <> "hello world2"
 
 def test_helloworld1_not_helloworld2_not_helloworld3():
     print("Running test_helloworld1_not_helloworld2_not_helloworld3")
