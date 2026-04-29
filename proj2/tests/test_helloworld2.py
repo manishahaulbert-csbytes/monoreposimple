262d5fdf8fae77d6a8a219ccc0577f9d605b6df1
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-04-29 15:01:38 +0200

    trigger quality gate failure proj2 block

--- proj2/tests/test_helloworld2.py
+++ proj2/tests/test_helloworld2.py
@@ -2,9 +2,9 @@ import proj2.helloworld2 as hw2
 
 def test_helloworld2():
     print("Running test_helloworld2")
-    assert hw2.j2 == "hello world2"
+    assert hw2.j2 != "hello world2"
 
 def test_not_helloworld2():
     print("Running test_not_helloworld2")
-    assert hw2.j2 != "goodbye world"
-    
+    assert hw2.j2 == "goodbye world"
+    print(f"Value of j2: {hw2.j2}")
