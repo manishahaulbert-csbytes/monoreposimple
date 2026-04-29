79c814a0bda3cffa9a750ea00546a0a13d2b3d02
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-04-29 14:11:40 +0200

    additional changes

--- proj1/tests/test_helloworld1.py
+++ proj1/tests/test_helloworld1.py
@@ -20,4 +20,5 @@ def test_helloworld1_not_helloworld2_not_helloworld3_not_helloworld4():
 
 def test_helloworld1_not_helloworld2_not_helloworld3_not_helloworld4_not_helloworld5():
     print("Running test_helloworld1_not_helloworld2_not_helloworld3_not_helloworld4_not_helloworld5")
+    print("This test should fail because j1 is not equal to 'hello world5'")
     assert hw.j1 != "hello world5"
