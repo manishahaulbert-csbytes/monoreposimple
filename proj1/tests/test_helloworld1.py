08e6427d9659f911a24054a6f0ec834f2b5e3782
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-04-29 15:19:32 +0200

    testing

--- proj1/tests/test_helloworld1.py
+++ proj1/tests/test_helloworld1.py
@@ -8,18 +8,18 @@ def test_not_helloworld1():
 
 def test_helloworld1_not_helloworld2():
     print("Running test_helloworld1_not_helloworld2")
-    assert hw.j1 != "hello world2"
+    assert hw.j1 == "hello world2"
 
 def test_helloworld1_not_helloworld2_not_helloworld3():
     print("Running test_helloworld1_not_helloworld2_not_helloworld3")
-    assert hw.j1 != "hello world3"
+    assert hw.j1 == "hello world3"
 
 def test_helloworld1_not_helloworld2_not_helloworld3_not_helloworld4():
     print("Running test_helloworld1_not_helloworld2_not_helloworld3_not_helloworld4")
-    assert hw.j1 != "hello world4"
+    assert hw.j1 == "hello world4"
 
 def test_helloworld1_not_helloworld2_not_helloworld3_not_helloworld4_not_helloworld5():
     print("Running test_helloworld1_not_helloworld2_not_helloworld3_not_helloworld4_not_helloworld5")
     print("This test should fail because j1 is not equal to 'hello world5'")
     print(f"Value of j1: {hw.j1}")
-    assert hw.j1 != "hello world5"
+    assert hw.j1 == "hello world5"
