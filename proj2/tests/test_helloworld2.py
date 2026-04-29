3f90549e35fa1d15b15393f726e3844aaf318a43
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-04-29 15:42:36 +0200

    test

--- proj2/tests/test_helloworld2.py
+++ proj2/tests/test_helloworld2.py
@@ -2,6 +2,7 @@ import proj2.helloworld2 as hw2
 
 def test_helloworld2():
     print("Running test_helloworld2")
+    print(f"Value of j2: {hw2.j2}")
     assert hw2.j2 != "hello world2"
 
 def test_not_helloworld2():
