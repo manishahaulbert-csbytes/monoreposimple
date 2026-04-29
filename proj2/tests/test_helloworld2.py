a161694b0e2e973399b9162a654c7d09a0f1dd38
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-04-29 18:36:45 +0200

    changes

--- proj2/tests/test_helloworld2.py
+++ proj2/tests/test_helloworld2.py
@@ -7,5 +7,5 @@ def test_helloworld2():
 
 def test_not_helloworld2():
     print("Running test_not_helloworld2")
-    assert hw2.j2 == "goodbye world"
+    assert hw2.j2 != "goodbye world"
     print(f"Value of j2: {hw2.j2}")
