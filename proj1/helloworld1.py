714e4c27961df2df6d7f68920fff160abd603e2f
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-05-01 14:26:38 +0200

    publish branchc

--- proj1/helloworld1.py
+++ proj1/helloworld1.py
@@ -1,2 +1,11 @@
-j1="hello world1"
-print(j1)
+
+def main():
+    j1="hello world1"
+    j2="hello world2"
+    print(j1)
+    print(j2)
+    if j1 <> j2:
+        print("j1 and j2 are different")
+
+if __name__ == "__main__":
+    main()
